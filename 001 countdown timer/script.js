c993ab3887f87d5c1695fea730456b40493be8fc
Neerajah <95176089+Neerajah@users.noreply.github.com>
2022-11-15 13:48:20 +0530

    Files added
    Files added

--- 001 countdown timer/script.js
+++ 001 countdown timer/script.js
@@ -1,20 +1,25 @@
-const days=document.querySelector('#days');
-const hours=document.querySelector('#hours');
-const minutes=document.querySelector('#minutes');
-const seconds=document.querySelector('#seconds');
-function updateTime(){
-const currentyear=new Date().getFullYear();
-const newYear=new Date('January 1 ${currentyear+1} 00:00:00');
-const currentYear=new Date();
-const diff=newYear-currentDate;
-const d=math.floor(diff/1000/60/60/24);
-const h=math.floor((diff/1000/60/60)%24);
-const m=math.floor((diff/1000/60)%60);
-const s=math.floor((diff/1000)%60);
-days.innerHTML=d<10?"0"+d:d;
-hours.innerHTML=h<10?"0"+h:h;
-minutes.innerHTML=m<10?"0"+m:m;
-seconds.innerHTML=s<10?"0"+s:s;
-}
+const days=document.querySelector("#days");
+const hours=document.querySelector("#hours");
+const minutes=document.querySelector("#minutes");
+const seconds=document.querySelector("#seconds");
+// const mseconds=document.querySelector("#mseconds");
 
-setInterval(updateTime,1000);
+const currentYear=new Date().getFullYear();
+const newYear=new Date(`January 1 ${currentYear+1} 00:00:00`);
+
+function UpdateTime(){
+  const currentDate=new Date();
+  const diff=newYear-currentDate;
+  const d=Math.floor(diff/1000/60/60/24);
+  const h=Math.floor((diff/1000/60/60)%24);
+  const m=Math.floor((diff/1000/60)%60);
+  const s=Math.floor((diff/1000)%60);
+  const ms=Math.floor(diff%1000);
+
+  days.innerHTML=d<10?"0"+d:d;
+  hours.innerHTML=h<10?"0"+h:h;
+  minutes.innerHTML=m<10?"0"+m:m;
+  seconds.innerHTML=s<10?"0"+s:s;
+  // mseconds.innerHTML=ms<10?"0"+ms:ms;
+}
+setInterval(UpdateTime,1000);
