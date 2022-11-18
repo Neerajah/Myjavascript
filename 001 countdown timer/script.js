ac2fcff01ce475fdf33a6022502905b2d4c4ceaa
Neerajah <95176089+Neerajah@users.noreply.github.com>
2022-11-18 10:13:11 +0530

    Files added
    Files added

--- 001 countdown timer/script.js
+++ 001 countdown timer/script.js
@@ -1,11 +1,16 @@
 const days=document.querySelector("#days");
+// document.getElementById('hours')
+// document.getElementsByClassName('newyear')
+// document.querySelector('#hours');
+// document.querySelectorAll('div');
+
 const hours=document.querySelector("#hours");
 const minutes=document.querySelector("#minutes");
 const seconds=document.querySelector("#seconds");
 // const mseconds=document.querySelector("#mseconds");
 
 const currentYear=new Date().getFullYear();
-const newYear=new Date(`January 1 ${currentYear+1} 00:00:00`);
+const newYear=new Date(`January 1 ${currentYear+1} 00:00:00`); 
 
 function UpdateTime(){
   const currentDate=new Date();
@@ -23,3 +28,15 @@ function UpdateTime(){
   // mseconds.innerHTML=ms<10?"0"+ms:ms;
 }
 setInterval(UpdateTime,1000);
+/* // primitive datatype (number, boolean, string, null, undefined), complex datatype (eg. object)
+var xyx = 90;// try to avoid
+let y; // undefined
+let x = 5;
+x = 5 * 5; //25
+x = x + 1; //26
+x = x - 6; //20
+
+const z = 10;
+
+z = 15; // throw error
+let b = z - 5; */
