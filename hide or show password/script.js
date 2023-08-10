eb2c676728e19de1dd7e1b122cd9b6ac190f3c1f
Neerajah <95176089+Neerajah@users.noreply.github.com>
2023-08-10 19:33:52 +0530

    Added projects
    Added projects

---
+++ hide or show password/script.js
@@ -0,0 +1,9 @@
+jQuery(".toggle-hide-show").click(function(){
+    jQuery(this).toggleClass("fa-eye fa-eye-slash");
+    input = jQuery(this).parent().find("input");
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+});
